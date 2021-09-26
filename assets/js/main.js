88aee6faa5b3355b63452f46f20373b5530c416e
rismanto02 <rismantoinformatika@gmail.com>
2021-09-26 22:27:56 +0700

    fix resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,7 +102,7 @@ function removeScale() {
     margin: 0,
     filename: "myResume.pdf",
     image: { type: "jpeg", quality: 0.98 },
-    html2canvas: { scale: 4 },
+    html2canvas: { scale: 5 },
     jsPDF: { format: "a4", orientation: "portrait" },
   };
   
